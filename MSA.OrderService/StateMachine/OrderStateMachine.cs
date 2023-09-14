762a0aa12cd99f98768a870a6b55b6d8629458b0
HARVEYNASH\nhutnguyenl <nhut.nguyenlam@nashtechglobal.com>
2023-09-14 11:58:36 +0700

    update state machine

--- MSA.OrderService/StateMachine/OrderStateMachine.cs
+++ MSA.OrderService/StateMachine/OrderStateMachine.cs
@@ -2,6 +2,7 @@
 using MSA.Common.Contracts.Settings;
 using MSA.Common.Contracts.Events.Order;
 using MSA.Common.Contracts.Events.Product;
+using MSA.Common.Contracts.Events.Payment;
 
 namespace MSA.OrderService.StateMachine;
 
@@ -36,11 +37,28 @@ public class OrderStateMachine
             x => x.CorrelateById(context => context.Message.OrderId)
         );
 
+        Event(() => PaymentProcessedSucceeded,
+            x => x.CorrelateById(context => context.Message.OrderId)
+        );
+
+        Event(() => PaymentProcessedFailed,
+            x => x.CorrelateById(context => context.Message.OrderId)
+        );
+
+        Event(() => OrderCompleted,
+            x => x.CorrelateById(context => context.Message.OrderId)
+        );
+
+        Event(() => OrderCancelled,
+            x => x.CorrelateById(context => context.Message.OrderId)
+        );
+
         Initially(
             When(OrderSubmitted)
                 .Then(x => Console.WriteLine($"Receiving Order {x.Message.OrderId}"))
                 .Then(x => {
                     x.Saga.OrderId = x.Message.OrderId;
+                    x.Saga.Reason += $"Receiving Order {x.Message.OrderId};";
                 })
                 .TransitionTo(Submitted)
         );
@@ -50,26 +68,75 @@ public class OrderStateMachine
                 .Then(x => Console.WriteLine($"Validate Product Succeeded for OrderId {x.Message.OrderId}"))
                 .Then(x => {
                     x.Saga.ProductValidationId = x.Message.ProductId;
+                    x.Saga.Reason += $"Validate Product Succeeded for OrderId {x.Message.OrderId};";
                 })
-                .TransitionTo(ValidatedSucceeded)
-                .Finalize(),
+                .TransitionTo(Processed),
             When(ProductValidatedFailed)
                 .Then(x => Console.WriteLine($"Validate Product Failed for OrderId {x.Message.OrderId}"))
                 .Then(x => {
                     x.Saga.ProductValidationId = x.Message.ProductId;
-                    x.Saga.Reason = x.Message.Reason;
+                    x.Saga.Reason += x.Message.Reason;
                 })
-                .TransitionTo(ValidatedSucceeded)
+                .TransitionTo(Cancelled)
+                .Finalize()
+        );
+        
+        During(Processed,
+            When(PaymentProcessedSucceeded)
+                .Then(x => Console.WriteLine($"Pay Succeeded for OrderId {x.Message.OrderId}"))
+                .Then(x => {
+                    x.Saga.PaymentId = x.Message.PaymentId;
+                    x.Saga.Reason = $"Pay Succeeded for OrderId {x.Message.OrderId}";
+                })
+                .Publish(context => new OrderCompleted()
+                {
+                    OrderId = context.Message.OrderId,
+                })
+                .TransitionTo(Confirmed),
+            When(PaymentProcessedFailed)
+                .Then(x => Console.WriteLine($"Pay Failed for OrderId {x.Message.OrderId}"))
+                .Then(x => {
+                    x.Saga.PaymentId = x.Message.PaymentId;
+                    x.Saga.Reason += x.Message.Reason;
+                })
+                .Publish(context => new OrderCancelled()
+                {
+                    OrderId = context.Message.OrderId,
+                })
+                .TransitionTo(Cancelled)
+        );
+
+        During(Confirmed,
+            When(OrderCompleted)
+                .Then(x => Console.WriteLine($"Order completed for OrderId {x.Message.OrderId}"))
+                .Then(x =>
+                {
+                    x.Saga.Reason += $"Order completed for OrderId {x.Message.OrderId}";
+                })
                 .Finalize()
         );
 
+        During(Cancelled,
+            When(OrderCancelled)
+                .Then(x => Console.WriteLine($"Order cancelled for OrderId {x.Message.OrderId}"))
+                .Then(x =>
+                {
+                    x.Saga.Reason += x.Message.Reason;
+                })
+                .Finalize()
+        );
     }
 
     public Event<OrderSubmitted> OrderSubmitted { get; private set; }
     public Event<ProductValidatedSucceeded> ProductValidatedSucceeded { get; private set; }
     public Event<ProductValidatedFailed> ProductValidatedFailed { get; private set; }
+    public Event<PaymentProcessedSucceeded> PaymentProcessedSucceeded { get; private set; }
+    public Event<PaymentProcessedFailed> PaymentProcessedFailed { get; private set; }
+    public Event<OrderCompleted> OrderCompleted { get; private set; }
+    public Event<OrderCancelled> OrderCancelled { get; private set; }
 
     public State Submitted { get; private set; }
-    public State ValidatedSucceeded { get; private set; }
-    public State ValidatedFailed { get; private set; }
+    public State Processed { get; private set; }
+    public State Cancelled { get; private set; }
+    public State Confirmed { get; private set; }
 }
